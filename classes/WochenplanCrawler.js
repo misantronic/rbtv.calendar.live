d37a5edb51c79fdc1f5e35ff45cd651ed8d852f1
david.schkalee <david.schkalee@sportalliance.com>
2021-12-24 22:40:34 +0100

    add end-time to VOD-episodes

--- classes/WochenplanCrawler.js
+++ classes/WochenplanCrawler.js
@@ -5,61 +5,89 @@ function WochenplanCrawler() {}
 
 WochenplanCrawler.prototype.onData = function () {};
 
+function fetch(url) {
+    return new Promise((resolve, reject) => {
+        https
+            .get(url, (resp) => {
+                const result = [];
+
+                resp.on("data", (chunk) => result.push(chunk));
+                resp.on("end", () => {
+                    const data = JSON.parse(result.join(""));
+
+                    resolve(data);
+                });
+            })
+            .on("error", (err) => {
+                console.log("Error: " + err.message);
+                reject(undefined);
+            });
+    });
+}
+
 function fetchData(url) {
-  return new Promise((resolve) => {
-    console.log("Crawling data from", url);
+    const data = (await fetch(url)).data;
 
-    https
-      .get(url, (resp) => {
-        const result = [];
+    return Promise.all(
+        data
+            .map(async (day) =>
+                day.elements.map(async (item) => {
+                    const startTime = new Date(
+                        item.timeStart || item.uploadDate
+                    );
+                    let endTime = item.timeEnd
+                        ? new Date(item.timeEnd)
+                        : undefined;
 
-        resp.on("data", (chunk) => result.push(chunk));
-        resp.on("end", () => {
-          const data = JSON.parse(result.join("")).data;
+                    if (!endTime) {
+                        const episode = await fetch(
+                            `https://api.rocketbeans.tv/v1/media/episode/${item.id}`
+                        );
 
-          resolve(
-            data
-              .map((day) =>
-                day.elements.map((item) => {
-                  const startTime = new Date(item.timeStart || item.uploadDate);
+                        if (episode) {
+                            const { duration } = episode.data.episodes[0];
 
-                  return {
-                    title: item.title,
-                    description: item.topic,
-                    startTime,
-                    endTime: item.timeEnd ? new Date(item.timeEnd) : startTime,
-                    type: item.type,
-                    image:
-                      item.episodeImage ||
-                      (item.showThumbnail
-                        ? item.showThumbnail[0].url
-                        : undefined),
-                    bohnen: (item.bohnen || []).map((bean) => bean.name)
-                  };
+                            endTime = new Date(
+                                endTime.getTime() + duration * 1000
+                            );
+                        }
+                    }
+
+                    if (!endTime) {
+                        endTime = startTime;
+                    }
+
+                    return {
+                        title: item.title,
+                        description: item.topic,
+                        startTime,
+                        endTime,
+                        type: item.type,
+                        image:
+                            item.episodeImage ||
+                            (item.showThumbnail
+                                ? item.showThumbnail[0].url
+                                : undefined),
+                        bohnen: (item.bohnen || []).map((bean) => bean.name),
+                    };
                 })
-              )
-              .reduce((memo, day) => [...memo, ...day], [])
-          );
-        });
-      })
-      .on("error", (err) => {
-        console.log("Error: " + err.message);
-      });
-  });
+            )
+            .reduce((memo, day) => [...memo, ...day], [])
+    );
 }
 
 WochenplanCrawler.prototype.start = async function () {
-  const startDay = Math.round(startOfDay(new Date()).getTime() / 1000);
-  const endDay = Math.round(endOfWeek(addWeeks(new Date(), 1)) / 1000);
+    const startDay = Math.round(startOfDay(new Date()).getTime() / 1000);
+    const endDay = Math.round(endOfWeek(addWeeks(new Date(), 1)) / 1000);
 
-  const dataLive = await fetchData(
-    `https://api.rocketbeans.tv/v1/schedule/normalized?startDay=${startDay}&endDay=${endDay}`
-  );
-  const dataVod = await fetchData(
-    `https://api.rocketbeans.tv/v1/schedule/publish?from=${startDay}`
-  );
+    const dataLive = await fetchData(
+        `https://api.rocketbeans.tv/v1/schedule/normalized?startDay=${startDay}&endDay=${endDay}`
+    );
+    const dataVod = await fetchData(
+        `https://api.rocketbeans.tv/v1/schedule/publish?from=${startDay}`
+    );
 
-  this.onData([...dataLive, ...dataVod]);
+    this.onData([...dataLive, ...dataVod]);
 };
 
 module.exports = WochenplanCrawler;
